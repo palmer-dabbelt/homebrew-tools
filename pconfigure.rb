54aaaa9573d566994da99a3b07072b6d14cdb42c
Palmer Dabbelt <palmer@dabbelt.com>
2017-01-17 15:28:10 -0800

    Add pconfigure formula

---
+++ pconfigure.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Pconfigure < Formula
+  url      'https://github.com/palmer-dabbelt/pconfigure/archive/v0.12.0.tar.gz'
+  homepage 'https://github.com/palmer-dabbelt/pconfigure/'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'talloc'
+  depends_on 'xz'
+
+  def install
+    system "sed", "-i", "bak", "s/soname/install_name/g", "Configfiles/main"
+    system "sed", "-i", "bak", "s/1024/10240/g", "src/pconfigure/main.c"
+    system "bash", "-ex", "./bootstrap.sh", "--prefix", "#{prefix}"
+    system "make", "install"
+  end
+end
