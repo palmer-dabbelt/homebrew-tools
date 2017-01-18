f50b6f8e45b30058da8d284e784a02f6e5967412
Palmer Dabbelt <palmer@dabbelt.com>
2017-01-17 17:47:24 -0800

    Update pconfigure, this works on OSX now

--- pconfigure.rb
+++ pconfigure.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Pconfigure < Formula
-  url      'https://github.com/palmer-dabbelt/pconfigure/archive/v0.12.0.tar.gz'
+  url      'https://github.com/palmer-dabbelt/pconfigure/archive/v0.12.1.tar.gz'
   homepage 'https://github.com/palmer-dabbelt/pconfigure/'
 
   depends_on 'pkg-config' => :build
@@ -9,6 +9,8 @@ class Pconfigure < Formula
   depends_on 'xz'
 
   def install
+    ENV.no_weak_imports
+
     system "sed", "-i", "bak", "s/soname/install_name/g", "Configfiles/main"
     system "sed", "-i", "bak", "s/1024/10240/g", "src/pconfigure/main.c"
     system "bash", "-ex", "./bootstrap.sh", "--prefix", "#{prefix}"
