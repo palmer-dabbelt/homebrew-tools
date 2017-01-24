a980ded277adc95ffc0eb59b8254e55d14ed1e73
Palmer Dabbelt <palmer@dabbelt.com>
2017-01-24 14:50:57 -0800

    Add libbase64 at 1.0.0_p3

---
+++ libbase64.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Libbase64 < Formula
+  url      'https://github.com/palmer-dabbelt/libbase64/archive/v1.0.0_p3.tar.gz'
+  version  '1.0.0_p3'
+  homepage 'https://github.com/palmer-dabbelt/libbase64/'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'cunit' => :build
+  depends_on 'autoconf' => :build
+  depends_on 'automake' => :build
+  depends_on 'libtool' => :build
+
+  def install
+    system "autoreconf", "-i"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+end
