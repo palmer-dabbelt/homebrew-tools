40be254ffe64fbc45f5064f04ac393e3cf03e629
Palmer Dabbelt <palmer@dabbelt.com>
2017-01-24 13:52:20 -0800

    Add psqlite at 0.0.5

---
+++ psqlite.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Psqlite < Formula
+  url      'https://github.com/palmer-dabbelt/psqlite/archive/v0.0.5.tar.gz'
+  homepage 'https://github.com/palmer-dabbelt/psqlite/'
+
+  depends_on 'palmer-dabbelt/tools/pconfigure' => :build
+  depends_on 'pkg-config' => :build
+  depends_on 'sqlite3'
+
+  def install
+    system "pconfigure", "--verbose", "PREFIX = #{prefix}"
+    system "make", "install"
+  end
+end
